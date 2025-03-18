e7e05eb3215b7f22620f0c876d273f94f843084f
Grammerc <Grammerc@>
2025-03-18 08:18:45 +0800

    Add project files.

---
+++ ClinicManagementSystemFinal/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ClinicManagementSystemFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            Dashboard = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label5 = new Label();
+            button1 = new Button();
+            pictureBox4 = new PictureBox();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Leftside;
+            pictureBox1.Location = new Point(-2, -1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(173, 117);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.Leftside;
+            pictureBox2.Location = new Point(754, -1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(173, 117);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.Leftside;
+            pictureBox3.Location = new Point(351, 113);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(576, 449);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // Dashboard
+            // 
+            Dashboard.AutoSize = true;
+            Dashboard.Cursor = Cursors.Hand;
+            Dashboard.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Dashboard.ForeColor = Color.Navy;
+            Dashboard.Location = new Point(351, 39);
+            Dashboard.Name = "Dashboard";
+            Dashboard.Size = new Size(95, 19);
+            Dashboard.TabIndex = 3;
+            Dashboard.Text = "Dashboard";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(476, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 19);
+            label1.TabIndex = 4;
+            label1.Text = "Doctors";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Navy;
+            label2.Location = new Point(564, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 19);
+            label2.TabIndex = 5;
+            label2.Text = "Services";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Navy;
+            label3.Location = new Point(664, 39);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 19);
+            label3.TabIndex = 6;
+            label3.Text = "Support";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.SlateGray;
+            label4.Location = new Point(-2, 171);
+            label4.Name = "label4";
+            label4.Size = new Size(355, 84);
+            label4.TabIndex = 7;
+            label4.Text = "Clinic Management\r\nSystem\r\n";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.MidnightBlue;
+            panel1.Location = new Point(370, 61);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(56, 3);
+            panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.MidnightBlue;
+            panel2.Location = new Point(52, 314);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(56, 3);
+            panel2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.SlateGray;
+            label5.Location = new Point(55, 343);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 20);
+            label5.TabIndex = 10;
+            label5.Text = "label5";
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderColor = Color.Navy;
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(177, 439);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 43);
+            button1.TabIndex = 11;
+            button1.Text = "Learn More";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(177, 12);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(107, 65);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 12;
+            pictureBox4.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Gold;
+            button2.FlatAppearance.BorderColor = Color.Navy;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(27, 439);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 43);
+            button2.TabIndex = 13;
+            button2.Text = "Get Started";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(925, 565);
+            Controls.Add(button2);
+            Controls.Add(pictureBox4);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Dashboard);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label Dashboard;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label5;
+        private Button button1;
+        private PictureBox pictureBox4;
+        private Button button2;
+    }
+}
